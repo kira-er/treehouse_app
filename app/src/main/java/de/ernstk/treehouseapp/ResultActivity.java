b32f7e779568b3b107e27e8478299a8316b1710f
kira-er <67024215+kira-er@users.noreply.github.com>
2020-10-06 20:26:20 +0200

    intent adjustments

--- app/src/main/java/de/ernstk/treehouseapp/ResultActivity.java
+++ app/src/main/java/de/ernstk/treehouseapp/ResultActivity.java
@@ -2,6 +2,7 @@ package de.ernstk.treehouseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ResultActivity extends AppCompatActivity {
@@ -10,18 +11,31 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        double size = intent.getDoubleExtra("size", 0);
+        int type = intent.getIntExtra("type", 0);
+        int amountPeople = intent.getIntExtra("amountPeople", 0);
+        double snowHeight = intent.getDoubleExtra("snow", 0);
+        double safetyFactor = intent.getDoubleExtra("safetyFactor", 1);
+
+        try {
+           double treeDiameter =  calculateDiameter(type, size, amountPeople, snowHeight, safetyFactor);
+        } catch (Exception e) {
+            finish();
+        }
     }
 
     private double calculateTreehouseWeight ( int type, double size) throws Exception {
         double treehouseWeightPerSqm;
         double treehouseWeight;
 
-            switch (type){
-            case 1:
+        switch (type){
+            case 0:
                 treehouseWeightPerSqm = 60; break;
-            case 2:
+            case 1:
                 treehouseWeightPerSqm = 125; break;
-            case 3:
+            case 2:
                 treehouseWeightPerSqm = 155; break;
             default:
                 throw new Exception("treehouse type is not valid");
@@ -49,16 +63,11 @@ public class ResultActivity extends AppCompatActivity {
     }
 
 
-    protected double calculateTotalWeight() throws Exception {
+    protected double calculateTotalWeight(int type, double size, int amountPeople, double snowHeight) throws Exception {
         double totalWeight;
         double treehouseWeight;
         double peopleWeight;
         double snowWeight;
-
-        int type = 2; // intent
-        double size = 30.5; // intent
-        int amountPeople = 5; // intent
-        double snowHeight = 2.5; //intent
 
         treehouseWeight = calculateTreehouseWeight(type, size);
         peopleWeight = calculatePeopleWeight(amountPeople);
@@ -68,15 +77,13 @@ public class ResultActivity extends AppCompatActivity {
         return totalWeight;
     }
 
-    protected double calculateDiameter() throws Exception {
+    protected double calculateDiameter(int type, double size, int amountPeople, double snowHeight, double safetyFactor) throws Exception {
         double totalWeight;
         double treeSqcm;
         double treeRadius;
         double treeDiameter;
 
-        double safetyFactor = 2; // intent
-
-        totalWeight = calculateTotalWeight();
+        totalWeight = calculateTotalWeight(type, size, amountPeople, snowHeight);
         treeSqcm = totalWeight/ 200;
         treeRadius = Math.sqrt(treeSqcm / Math.PI);
         treeDiameter = treeRadius * 2;
