d277fc6e8d9ce56216b13607f694fb3ac24bb873
kira-er <67024215+kira-er@users.noreply.github.com>
2020-10-06 18:55:29 +0200

    formula implementation

--- app/src/main/java/de/ernstk/treehouseapp/ResultActivity.java
+++ app/src/main/java/de/ernstk/treehouseapp/ResultActivity.java
@@ -11,4 +11,82 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
     }
+
+    private double calculateTreehouseWeight ( int type, double size) throws Exception {
+        double treehouseWeightPerSqm;
+        double treehouseWeight;
+
+            switch (type){
+            case 1:
+                treehouseWeightPerSqm = 60; break;
+            case 2:
+                treehouseWeightPerSqm = 125; break;
+            case 3:
+                treehouseWeightPerSqm = 155; break;
+            default:
+                throw new Exception("treehouse type is not valid");
+        }
+        treehouseWeight = treehouseWeightPerSqm * size;
+        return treehouseWeight;
+    }
+
+
+    private double calculatePeopleWeight (int amountPeople) {
+        double weightPerson = 85; //average weight of a man in Germany
+        double weightPeople;
+
+        weightPeople = amountPeople * weightPerson;
+        return weightPeople;
+    }
+
+
+    private double calculateSnowWeight(double size, double snowHeight) {
+        double snowWeight;
+        int snowWeightPerSqm = 4; // weight of 1cm snow per 1m^2
+
+        snowWeight = snowHeight * snowWeightPerSqm * size;
+        return snowWeight;
+    }
+
+
+    protected double calculateTotalWeight() throws Exception {
+        double totalWeight;
+        double treehouseWeight;
+        double peopleWeight;
+        double snowWeight;
+
+        int type = 2; // intent
+        double size = 30.5; // intent
+        int amountPeople = 5; // intent
+        double snowHeight = 2.5; //intent
+
+        treehouseWeight = calculateTreehouseWeight(type, size);
+        peopleWeight = calculatePeopleWeight(amountPeople);
+        snowWeight = calculateSnowWeight(size, snowHeight);
+
+        totalWeight = treehouseWeight + peopleWeight + snowWeight;
+        return totalWeight;
+    }
+
+    protected double calculateDiameter() {
+        double totalWeight;
+        double treeSqcm;
+        double treeRadius;
+        double treeDiameter;
+
+        double safetyFactor = 2; // intent
+        
+        try {
+            totalWeight = calculateTotalWeight();
+            treeSqcm = totalWeight/ 200;
+            treeRadius = Math.sqrt(treeSqcm / Math.PI);
+            treeDiameter = treeRadius * 2;
+            treeDiameter = treeDiameter * safetyFactor;
+            return treeDiameter;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
